670f223ee44879deb257562d09ac113d5d0c8774
FioKron <jmor@hotmail.co.uk>
2017-09-26 15:17:43 +0100

    Guide 02a Exercise 1 Complete
    For basic testing of debugging.

---
+++ debugtest.cpp
@@ -0,0 +1,20 @@
+
+/**
+	int main()
+
+	Description: Application Entry Point, for running
+	this test debugging application.
+
+	@Return int: an execution code (set to 0
+	in this case).
+*/
+int main()
+{
+	int a, b, c;
+
+	a = 10;
+	b = 2;
+	c = a / b;
+
+	return 0;
+}
